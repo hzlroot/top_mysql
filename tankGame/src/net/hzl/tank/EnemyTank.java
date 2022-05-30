106a63506d2ec372ab0b5004af362571025fc3e6
黄志林 <2990316439@qq.com>
2022-05-30 22:39:18 +0800

    这是第一次提交坦克大战代码

---
+++ tankGame/src/net/hzl/tank/EnemyTank.java
@@ -0,0 +1,7 @@
+package net.hzl.tank;
+
+public class EnemyTank extends Tank {
+    public EnemyTank(int x, int y) {
+        super(x, y);
+    }
+}
---
+++ tankGame/src/net/hzl/tank/MyTank.java
@@ -0,0 +1,11 @@
+package net.hzl.tank;
+
+public class MyTank extends Tank {
+
+
+    public MyTank(int x, int y) {
+        super(x, y);
+    }
+
+
+}
